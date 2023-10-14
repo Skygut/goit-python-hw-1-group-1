1ffbea632f896ac56b532716377a39a2f340e4c8
Skygut <v.chub@outlook.de>
2023-10-14 19:34:14 +0200

    New changes

--- main.py
+++ main.py
@@ -7,26 +7,21 @@ def main():
     while True:
         user_input = input("Enter a command: ")
         command, *args = function.parse_input(user_input)
-
-        if command in ["close", "exit", "good bye"]:
+        if command in ["close", "exit", "bye"]:
             print("Good bye!")
             break
         elif command == "hello":
             print("How can I help you?")
-            continue
         elif command == "phone":
             print(function.show_phone(args, contacts))
-            continue
         elif command == "all":
             print(function.show_all(args, contacts))
-            continue
-        if command not in ("add", "change"):
-            print("Invalid command.")
-
-        if command == "add":
+        elif command == "add":
             print(function.add_contact(args, contacts))
         elif command == "change":
             print(function.change_contact(args, contacts))
+        else:
+            print("Invalid command.")
 
 
 if __name__ == "__main__":
